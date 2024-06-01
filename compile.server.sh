19df32d79e41f6ed984546c622fc8bb61ac663c9
Azure <azurexsx@gmail.com>
2024-06-01 18:07:18 +0200

    Server compile script update

--- compile.server.sh
+++ compile.server.sh
@@ -5,7 +5,10 @@ export LD_LIBRARY_PATH=./MsQuic/bin:$LD_LIBRARY_PATH
 if ! g++ -g ./server/server.cpp -o./build/server \
 	-L./MsQuic/bin -lmsquic \
 	-lstdc++ \
-	./server/QuicServer.cpp; then 
+	./server/QuicServer.cpp \
+	./Entities/Message.cpp \
+	./Entities/User.cpp \
+	./Entities/Contact.cpp; then 
 	echo "Error: Compilation failed"
 	exit 1
 fi
