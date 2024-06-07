45d40670b8573ff4d2a30118bfcd1e82d4c50bb5
Azure <azurexsx@gmail.com>
2024-06-07 17:38:11 +0200

    cleanup

--- compile.client.sh
+++ compile.client.sh
@@ -1,13 +1,5 @@
 #!/bin/bash 
 
-export SRC_PROTO_DIR=./proto
-export DST_PROTO_DIR=./proto
-
-if ! protoc -I=$SRC_PROTO_DIR --cpp_out=$DST_PROTO_DIR $SRC_PROTO_DIR/test.proto; then
-    echo "Error: Proto failed"
-    exit 1
-fi
-
 if ! g++ -g -o ./build/client \
     -I./proto \
     -I/usr/include/gtk-4.0 \
--- compile.server.sh
+++ compile.server.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-export SRC_PROTO_DIR=./proto
-export DST_PROTO_DIR=./proto
-
-if ! protoc -I=$SRC_PROTO_DIR --cpp_out=$DST_PROTO_DIR $SRC_PROTO_DIR/test.proto; then
-    echo "Error: Proto failed"
-    exit 1
-fi
-
 if ! g++ -g ./server/server.cpp -o./build/server \
 	-I./proto \
 	-L./MsQuic/bin -lmsquic \
