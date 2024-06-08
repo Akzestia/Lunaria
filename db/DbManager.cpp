804f6eea1f94db00d07b9b3b1c29a4b3854f0748
Azure <azurexsx@gmail.com>
2024-06-08 16:56:43 +0200

    Fix looping over pqxx::result rows(switched to iterators)

--- db/DbManager.cpp
+++ db/DbManager.cpp
@@ -1,25 +1,30 @@
 #include "DbManager.h"
 
-void DbManager::test(){
+void DbManager::test() {
     try {
         const std::string connection_str = DbManager::getConnectionString();
-        
+
         pqxx::connection connection(connection_str);
-        
+
         if (connection.is_open()) {
-            std::cout << "Connected to database successfully: " << connection.dbname() << std::endl;
+            std::cout << "Connected to database successfully: "
+                      << connection.dbname() << std::endl;
         } else {
             std::cerr << "Can't open database" << std::endl;
         }
-        
+
         pqxx::nontransaction nontransaction(connection);
-        
-        pqxx::result result = nontransaction.exec("SELECT version();");
-        
-        for (auto row : result) {
-            std::cout << row[0].c_str() << std::endl;
+
+        pqxx::result result = nontransaction.exec("SELECT * FROM users;");
+
+        pqxx::result::const_iterator row_it = result.begin();
+        pqxx::result::const_iterator row_end = result.end();
+
+        for (; row_it != row_end; ++row_it) {
+            const pqxx::result::const_iterator row = *row_it;
+            std::cout << row[1].c_str() << std::endl;
         }
-        
+
         connection.close();
     } catch (const std::exception &e) {
         std::cerr << e.what() << std::endl;
@@ -36,13 +41,13 @@ std::string DbManager::getConnectionString() {
         std::string db_name = pt.get<std::string>("DB_NAME");
         std::string db_password = pt.get<std::string>("DB_PASSWORD");
 
-        std::string connection_string = "host=" + db_host + 
-                                        " user=" + db_user + 
-                                        " dbname=" + db_name + 
+        std::string connection_string = "host=" + db_host + " user=" + db_user +
+                                        " dbname=" + db_name +
                                         " password=" + db_password;
 
         return connection_string;
-    } catch (const boost::property_tree::ptree_error& e) {
-        throw std::runtime_error("One or more configuration variables are not set.");
+    } catch (const boost::property_tree::ptree_error &e) {
+        throw std::runtime_error(
+            "One or more configuration variables are not set.");
     }
 }
