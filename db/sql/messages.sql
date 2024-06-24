7e445a6d0733d34adb259d3a0807d6c641712329
Azure <azurexsx@gmail.com>
2024-06-24 09:43:46 +0200

    Message sql script update

---
+++ db/sql/messages.sql
@@ -0,0 +1,11 @@
+CREATE TABLE Messages
+(
+    id SERIAL PRIMARY KEY,
+    sender_id INT NOT NULL,
+    receiver_id INT NOT NULL,
+    text_content VARCHAR(2000),
+    byte_content BYTEA,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    FOREIGN KEY (sender_id) REFERENCES Users(id),
+    FOREIGN KEY (receiver_id) REFERENCES Users(id)
+)
