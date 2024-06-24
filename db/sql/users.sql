f639137d47e0eedca3fae05d8cab17ff3bee0152
Azure <azurexsx@gmail.com>
2024-06-24 09:46:50 +0200

    User sql script update

---
+++ db/sql/users.sql
@@ -0,0 +1,8 @@
+CREATE TABLE Users
+(
+    id SERIAL PRIMARY KEY,
+    user_name VARCHAR(50) NOT NULL
+    user_email VARCHAR(200) NOT NULL
+    user_avatar BYTEA,
+    user_password VARCHAR(200) NOT NULL
+)
