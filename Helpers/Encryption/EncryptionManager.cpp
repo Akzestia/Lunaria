1aad8d1d99b27d4f3d4f5dc8c103dab3dbeffae4
Azure <azurexsx@gmail.com>
2024-06-29 09:42:14 +0200

    EncryptionManager::generateKey method implemented

--- Helpers/Encryption/EncryptionManager.cpp
+++ Helpers/Encryption/EncryptionManager.cpp
@@ -38,4 +38,24 @@ bool EncryptionManager::decryptMessage(const char* encryptedMessage, const char*
     return true;
 }
 
+bool EncryptionManager::generateKey (const char* userName, char** output){
+if (userName == nullptr || output == nullptr)
+        return false;
 
+    size_t userNameLength = strlen(userName);
+
+    unsigned int hash = 0;
+    for (size_t i = 0; i < userNameLength; ++i) {
+        hash = (hash * 31) + userName[i];
+    }
+
+    char hashString[16];
+    snprintf(hashString, sizeof(hashString), "%u", hash);
+
+    *output = (char*)malloc(strlen(hashString) + 1);
+    if (*output == nullptr)
+        return false;
+
+    strcpy(*output, hashString);
+    return true;
+}
--- Helpers/Encryption/EncryptionManager.h
+++ Helpers/Encryption/EncryptionManager.h
@@ -1,5 +1,6 @@
 #include <cstring>
 #include <cstdlib>
+#include <cstdio>
 
 class EncryptionManager
 {
