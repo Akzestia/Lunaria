eaa157819bb02670ed7a201731322f0a7ff51074
Azure <azurexsx@gmail.com>
2024-06-29 09:48:03 +0200

    Added sql script for ecnryption keys

---
+++ db/sql/encrypt_keys.sql
@@ -0,0 +1,10 @@
+CREATE TABLE
+    Encrypt_keys (
+        id SERIAL NOT NULL PRIMARY KEY,
+        _key VARCHAR(32) NOT NULL,
+        _user_id INT NOT NULL,
+        FOREIGN KEY (_user_id) REFERENCES Users (id),
+        CONSTRAINT unique_user_id UNIQUE (_user_id)
+    )
+
+CREATE INDEX idx_user_id ON Encrypt_keys(_user_id);
