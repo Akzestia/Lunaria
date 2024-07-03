5ea65afa125125e3c8df9e2ef3ac1f24511fea5e
Azure <azurexsx@gmail.com>
2024-07-03 15:59:26 +0200

    RouteManager update

--- route-manager/RouteManager.h
+++ route-manager/RouteManager.h
@@ -2,14 +2,17 @@
 #include <functional>
 #include <unordered_map>
 #include "../proto/build/wrapper.pb.h"
-
+#include "../proto/build/contact.pb.h"
+#include "../proto/build/message.pb.h"
+#include "../proto/build/user.pb.h"
 
 class RouteManager {
   public:
   protected:
-    bool proccesRoute();
+    bool proccesRoute(const Wrapper&);
     virtual ~RouteManager();
   private:
-    using RouteFunction = std::function<bool(const Wrapper&)>;
+  using Payload = std::variant<User, Message, Contact>;
+    using RouteFunction = std::function<bool(const Payload&)>;
     static std::unordered_map<uint8_t, RouteFunction>* routes;
 };
