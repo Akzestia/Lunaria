ffd933d65d01d746cd125d2fbe89c79384fb9715
Azure <azurexsx@gmail.com>
2024-07-04 16:14:10 +0200

    RouteManager update

--- route-manager/RouteManager.h
+++ route-manager/RouteManager.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "../db/DbManager.h"
 #include "../proto/build/auth.pb.h"
 #include "../proto/build/contact.pb.h"
 #include "../proto/build/encrypt_key.pb.h"
@@ -10,19 +11,29 @@
 #include "../proto/build/vpn_graph.pb.h"
 #include "../proto/build/wrapper.pb.h"
 #include <functional>
+#include <set>
 #include <unordered_map>
 
 using Payload = std::variant<User, Message, Contact, Auth, Vpn_graph, Server,
                              Report, Invite_link, Encrypt_key>;
 using RouteFunction = std::function<bool(const Payload &)>;
 
-class RouteManager {
+class RouteManager : protected DbManager {
   public:
   protected:
     static bool proccesRoute(const Wrapper &);
     virtual ~RouteManager();
+
   private:
-    static bool handleAuth(const Payload&);
-    static bool handleReport(const Payload&);
+    static bool handleAuth(const Payload &);
+    static bool handleReport(const Payload &);
+
+    static bool getMessages(const Payload &, std::set<Message> &);
+    static bool getContacts(const Payload &, std::set<User> &);
+    static bool getServers(const Payload &, std::set<Server> &);
+
+    static bool updateUser(const Payload &);
+    static bool updateServer(const Payload &);
+
     static std::unordered_map<uint8_t, RouteFunction> *routes;
 };
