5d7c9ffa13a2bd808a1e3df9658839aa41a2cd10
Azure <azurexsx@gmail.com>
2024-07-06 15:49:51 +0200

    Cleanup

--- client/clientRequestManager/RequestManager.cpp
+++ client/clientRequestManager/RequestManager.cpp
@@ -1,7 +1,7 @@
 #include "RequestManager.h"
 
 Lxcode RequestManager::StartSignUpRequest(const Auth &auth) {
-    Lxcode code = {true, 0x00};
+    Lxcode code = {true, SUCCESS};
 
     if (!auth.has_sign_up())
         return {false, 0x81};
@@ -21,5 +21,28 @@ Lxcode RequestManager::StartSignUpRequest(const Auth &auth) {
 
     return code;
 };
+
+Lxcode RequestManager::StartSignInRequest(const Auth &auth) {
+    Lxcode code = {true, 0x00};
+
+    if (!auth.has_sign_up())
+        return {false, 0x81};
+
+    User u;
+    u.set_user_name(auth.sign_up().user_name());
+    u.set_user_display_name(auth.sign_up().display_name());
+    u.set_user_email(auth.sign_up().user_email());
+    u.set_user_password(auth.sign_up().user_password());
+
+    if (!DbManager::getUser(auth).is_successful) {
+        code.is_successful = false;
+        code.error_code = 0x82;
+    }
+
+    printf("\nUser was signed up successfully");
+
+    return code;
+};
+
 
 RequestManager::~RequestManager() {}
