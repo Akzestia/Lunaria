2e47af797f789166fdcaa651e2d2f569b72b1cf1
Azure <azurexsx@gmail.com>
2024-07-16 23:48:57 +0200

    AuthManager update

--- Helpers/AuthManager/AuthManager.cpp
+++ Helpers/AuthManager/AuthManager.cpp
@@ -6,12 +6,26 @@ AuthManager::AuthManager() {
 AuthManager::~AuthManager() {
 }
 
-bool AuthManager::verifyToken(const char* token) {
-    return strcmp(token, this->secretKey) == 0;
+bool AuthManager::verifyToken(const char* username, const char* password, const char* token) {
+    const char* generatedToken = generateToken(username, password);
+    bool result = strcmp(generatedToken, token) == 0;
+    delete[] generatedToken;
+    return result;
 }
 
-const char* AuthManager::generateToken(const char* username) {
-    return this->secretKey;
+const char* AuthManager::generateToken(const char* username, const char* password) {
+    std::string data = std::string(username) + password;
+
+    unsigned char* digest;
+    digest = HMAC(EVP_sha256(), secretKey, strlen(secretKey), 
+                  reinterpret_cast<const unsigned char*>(data.c_str()), data.length(), NULL, NULL);
+
+    char* hexDigest = new char[SHA256_DIGEST_LENGTH*2 + 1];
+    for (int i = 0; i < SHA256_DIGEST_LENGTH; ++i) {
+        sprintf(&hexDigest[i*2], "%02x", (unsigned int)digest[i]);
+    }
+
+    return hexDigest;
 }
 
 const char* AuthManager::getSecretKey() {
--- Helpers/AuthManager/AuthManager.h
+++ Helpers/AuthManager/AuthManager.h
@@ -1,15 +1,18 @@
 #ifndef AUTH_MANAGER_H
 #define AUTH_MANAGER_H
 #include <cstring>
+#include <openssl/hmac.h>
+#include <openssl/sha.h>
+#include <string>
 
 class AuthManager {
 private:
-    const char* secretKey;
+    const char* secretKey = "my_secret";
 public:
     AuthManager();
     ~AuthManager();
-    bool verifyToken(const char* token);
-    const char* generateToken(const char* username);
+    bool verifyToken(const char*, const char*, const char*);
+    const char* generateToken(const char*, const char*);
     const char* getSecretKey();
 };
 
