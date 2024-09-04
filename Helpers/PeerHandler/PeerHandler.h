0268434ac16f701d74664c33d5327964fedcf446
Azure <azurexsx@gmail.com>
2024-09-04 22:08:06 +0200

    Fixed redefinition errors

--- Helpers/PeerHandler/PeerHandler.h
+++ Helpers/PeerHandler/PeerHandler.h
@@ -1,9 +1,6 @@
 #ifndef PEER_HANDLER_H
 #define PEER_HANDLER_H
 #include "../../MsQuic/Linux_x64/include/msquic.h"
-#include "../../proto/build/test.pb.h"
-#include "../../proto/build/user.pb.h"
-#include "../../proto/build/wrapper.pb.h"
 #include "../../route-manager/RouteManager.h"
 #include "../../proto/build/authResponse.pb.h"
 #include <absl/strings/cord.h>
