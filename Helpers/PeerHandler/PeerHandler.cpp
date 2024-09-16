49c4764a68bf855e859e808da55aa82f55aa7d56
Azure <azurexsx@gmail.com>
2024-09-16 22:06:41 +0200

    Implemented new rpc route for fetching contacts. Enhanced memory management by allocating all objects inside arena (except Lxcode)

--- Helpers/PeerHandler/PeerHandler.cpp
+++ Helpers/PeerHandler/PeerHandler.cpp
@@ -1,12 +1,14 @@
 #include "PeerHandler.h"
-#include "../../route-manager/Routes.hpp"
-#include "../../proto/build/rpc_request.pb.h"
 #include "../../proto/build/rpc_body.pb.h"
+#include "../../proto/build/rpc_request.pb.h"
+#include "../../route-manager/Routes.hpp"
 #include "../../server/QuicServer.h"
 #include <cstdio>
 #include <google/protobuf/arena.h>
+#include <google/protobuf/repeated_field.h>
 #include <iostream>
 #include <memory>
+#include <vector>
 
 std::unordered_map<HQUIC, uint8_t *> *PeerHandler::peers =
     new std::unordered_map<HQUIC, uint8_t *>();
@@ -54,44 +56,53 @@ bool PeerHandler::onPeerShutdown(HQUIC Stream, void *context) {
         return false;
     }
 
-    delete [] data;
+    delete[] data;
     peers->erase(Stream);
     peerDataSizes->erase(Stream);
-    if(request->has_error()){
+    if (request->has_error()) {
         printf("Error proccessign request");
         return false;
     }
 
     std::cout << "\nRoute: " << request->route() << "\n";
-    std::cout << "\nPassword " << request->body().si_request().user_password() << "\n";
+    std::cout << "\nPassword " << request->body().si_request().user_password()
+              << "\n";
 
     switch (request->route()) {
     case SIGN_UP: {
-
-        if(!request->has_body() || !request->body().has_su_request())
-            return false;
-        SignUpRequest su = request->body().su_request();
-
         Arena arena;
-        Lxcode response = RouteManager::handleSignUp(su, arena);
+        Response *rpc_response =
+            google::protobuf::Arena::Create<Response>(&arena);
+        if (!request->has_body() || !request->body().has_su_request()) {
+            *rpc_response->mutable_error() = Error();
 
-        Response rpc_response;
+            reinterpret_cast<QuicServer *>(context)->SendResponse(
+                Stream, *rpc_response);
+            std::cout << "SignInX failed\n";
+            return false;
+        }
+
+        Lxcode response =
+            RouteManager::handleSignUp(request->body().su_request(), arena);
+
         if (response == Lxcode::OK()) {
             std::cout << "Sign up successful\n";
-            rpc_response.set_route(AUTH_RESPONSE_SIGN_UP);
-            AuthResponse authResponse;
+            rpc_response->set_route(AUTH_RESPONSE_SIGN_UP);
+            AuthResponse *authResponse =
+                google::protobuf::Arena::Create<AuthResponse>(&arena);
 
             try {
-                authResponse.set_is_successful(true);
-                authResponse.set_token(response.response);
-                *authResponse.mutable_user() = *std::get<User*>(response.payload);
+                authResponse->set_is_successful(true);
+                authResponse->set_token(response.response);
+                *authResponse->mutable_user() =
+                    *std::get<User *>(response.payload);
 
-                Body rpc_body;
-                *rpc_body.mutable_au_response() = authResponse;
-                *rpc_response.mutable_body() = rpc_body;
+                Body *rpc_body = google::protobuf::Arena::Create<Body>(&arena);
+                *rpc_body->mutable_au_response() = *authResponse;
+                *rpc_response->mutable_body() = *rpc_body;
 
                 reinterpret_cast<QuicServer *>(context)->SendResponse(
-                    Stream, rpc_response);
+                    Stream, *rpc_response);
                 return true;
             } catch (const std::exception &e) {
                 std::cerr << e.what() << '\n';
@@ -100,85 +111,149 @@ bool PeerHandler::onPeerShutdown(HQUIC Stream, void *context) {
 
         std::cout << "Sign up failed\n";
 
-        *rpc_response.mutable_error() = Error();
+        *rpc_response->mutable_error() = Error();
 
         reinterpret_cast<QuicServer *>(context)->SendResponse(Stream,
-            rpc_response);
+                                                              *rpc_response);
 
         return false;
     }
     case SIGN_IN: {
+        Arena arena;
+        Response *rpc_response =
+            google::protobuf::Arena::Create<Response>(&arena);
 
-        if(!request->has_body() || !request->body().has_si_request())
+        if (!request->has_body() || !request->body().has_si_request()) {
+            *rpc_response->mutable_error() = Error();
+
+            reinterpret_cast<QuicServer *>(context)->SendResponse(
+                Stream, *rpc_response);
+            std::cout << "SignInX failed\n";
             return false;
+        }
 
         printf("SignInX");
-        Arena arena;
-        Lxcode response = RouteManager::handleSignIn(request->body().si_request(), arena);
 
-        Response rpc_response;
+        Lxcode response =
+            RouteManager::handleSignIn(request->body().si_request(), arena);
+
         if (response == Lxcode::OK()) {
-            AuthResponse authResponse;
+            AuthResponse *authResponse =
+                google::protobuf::Arena::Create<AuthResponse>(&arena);
 
-            rpc_response.set_route(AUTH_RESPONSE_SIGN_IN);
+            rpc_response->set_route(AUTH_RESPONSE_SIGN_IN);
 
             std::cout << "Sign in successful\n";
 
-            authResponse.set_is_successful(true);
-            authResponse.set_token(response.response);
+            authResponse->set_is_successful(true);
+            authResponse->set_token(response.response);
 
-            *authResponse.mutable_user() = *std::get<User *>(response.payload);
+            *authResponse->mutable_user() = *std::get<User *>(response.payload);
 
-            Body rpc_body;
-            *rpc_body.mutable_au_response() = authResponse;
-            *rpc_response.mutable_body() = rpc_body;
+            Body *rpc_body = google::protobuf::Arena::Create<Body>(&arena);
+            *rpc_body->mutable_au_response() = *authResponse;
+            *rpc_response->mutable_body() = *rpc_body;
 
             reinterpret_cast<QuicServer *>(context)->SendResponse(
-                Stream, rpc_response);
+                Stream, *rpc_response);
             return true;
         }
 
         std::cout << "Sign in failed\n";
 
-        *rpc_response.mutable_error() = Error();
+        *rpc_response->mutable_error() = Error();
 
-        reinterpret_cast<QuicServer *>(context)->SendResponse(
-            Stream, rpc_response);
+        reinterpret_cast<QuicServer *>(context)->SendResponse(Stream,
+                                                              *rpc_response);
         return false;
     }
     case CREATE_CONTACT: {
 
-        if(!request->has_body() || !request->body().has_ct_request())
-            return false;
+        std::cout << "Creation started\n";
 
         Arena arena;
-        Lxcode response = RouteManager::createContact(request->body().ct_request(), arena);
-        Response rpc_response;
+        Response *rpc_response =
+            google::protobuf::Arena::Create<Response>(&arena);
+
+        if (!request->has_body() || !request->body().has_ct_request()) {
+            *rpc_response->mutable_error() = Error();
+
+            reinterpret_cast<QuicServer *>(context)->SendResponse(
+                Stream, *rpc_response);
+            std::cout << "Contact creation failed\n";
+            return false;
+        }
+        std::cout << "Arena started\n";
+        Lxcode response =
+            RouteManager::createContact(request->body().ct_request(), arena);
+
         if (response == Lxcode::OK()) {
             std::cout << "Contact created\n";
 
-            rpc_response.set_route(POST_RESPONSE_CONTACT);
+            rpc_response->set_route(POST_RESPONSE_CONTACT);
             Body rpc_body;
-            *rpc_body.mutable_ct_response() = *std::get<Contact*>(response.payload);
+            *rpc_body.mutable_ct_response() =
+                *std::get<Contact *>(response.payload);
+            *rpc_response->mutable_body() = rpc_body;
+
             reinterpret_cast<QuicServer *>(context)->SendResponse(
-                Stream, rpc_response);
+                Stream, *rpc_response);
         }
 
-        *rpc_response.mutable_error() = Error();
+        *rpc_response->mutable_error() = Error();
 
-        reinterpret_cast<QuicServer *>(context)->SendResponse(
-            Stream, rpc_response);
+        reinterpret_cast<QuicServer *>(context)->SendResponse(Stream,
+                                                              *rpc_response);
         std::cout << "Contact creation failed\n";
 
         return false;
     }
-    case FETCH_CONTACTS_BY_ID:{
-        if(!request->has_body() || !request->body().has_f_contacts())
-            return false;
+    case FETCH_CONTACTS_BY_ID: {
+
+        std::cout << "Fetching contacts...\n";
 
         Arena arena;
-        const char* user_id = request->body().f_contacts().user_id().c_str();
+        Response *rpc_response =
+            google::protobuf::Arena::Create<Response>(&arena);
+
+        if (!request->has_body() || !request->body().has_f_contacts()) {
+            *rpc_response->mutable_error() = Error();
+
+            reinterpret_cast<QuicServer *>(context)->SendResponse(
+                Stream, *rpc_response);
+            std::cout << "Fetching contacts failed\n";
+            return false;
+        }
+
+        const char *user_id = request->body().f_contacts().user_id().c_str();
         Lxcode response = RouteManager::getContacts(user_id, arena);
+
+        if (response == Lxcode::OK()) {
+            std::cout << "Contacts fetched\n";
+
+            rpc_response->set_route(FETCH_CONTACTS_RESPONSE);
+            Body *rpc_body = google::protobuf::Arena::Create<Body>(&arena);
+            FetchResponseContacts *f_c =
+                google::protobuf::Arena::Create<FetchResponseContacts>(&arena);
+
+            auto* ptr = std::get<ArenaVector<User>*>(response.payload);
+            google::protobuf::RepeatedPtrField<User>* field = f_c->mutable_response();
+            field->Assign(ptr->begin(), ptr->end());
+
+            *rpc_body->mutable_f_contacts_response() = *f_c;
+            *rpc_response->mutable_body() = *rpc_body;
+
+            reinterpret_cast<QuicServer *>(context)->SendResponse(
+                Stream, *rpc_response);
+        }
+
+        *rpc_response->mutable_error() = Error();
+
+        reinterpret_cast<QuicServer *>(context)->SendResponse(Stream,
+                                                              *rpc_response);
+        std::cout << "Fetching contacts failed\n";
+
+        return false;
     }
     default:
         std::cerr << "Error: Unknown route" << std::endl;
